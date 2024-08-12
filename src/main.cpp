9de3099c184228d1c5c8f9774c7558c481828201
tomas.lajda <tomas.lajda0@gmail.com>
2024-08-12 16:54:48 +0200

    creating a window with version 3.3

--- src/main.cpp
+++ src/main.cpp
@@ -3,4 +3,19 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-int main() { return 0; }
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  return 0;
+}

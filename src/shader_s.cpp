0b84abcaf0757280b79e47e01077c78de38a6404
tomas.lajda <tomas.lajda0@gmail.com>
2024-08-26 22:46:29 +0200

    edited vertex shader so the color copies the vertex

--- src/shader_s.cpp
+++ src/shader_s.cpp
@@ -73,7 +73,7 @@ void Shader::setInt(const std::string &name, int value) const {
 }
 
 void Shader::setFloat(const std::string &name, float value) const {
-  glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+  glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 }
 
 void Shader::checkCompileErrors(unsigned int shader, std::string type) {
